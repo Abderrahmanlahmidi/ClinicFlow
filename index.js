f03d6115e37ebe2b0a14912570917706ea32cd56
lahmidi <dvabderrahman@gmail.com>
2025-10-12 08:56:23 +0200

    add changes

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 
-
-
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 
 const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Auth API",
-      version: "1.0.0",
-      description: "Documentation for Login & Register routes",
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Auth API",
+            version: "1.0.0",
+            description: "Documentation for Login & Register routes",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
     },
-    servers: [
-      {
-        url: "http://localhost:3000", 
-      },
-    ],
-  },
-  apis: ["./routes/**/*.js"], 
+    apis: ["./routes/**/*.js"],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
@@ -35,26 +33,22 @@ const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error);
-})
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
-  
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
-const Main = require('./routes/routes');
-const Register = require('./routes/auth/register');
-const Login = require('./routes/auth/login');
 
-app.use('/api', Main);
-app.use('/api/auth', Register);
-app.use('/api/auth', Login);
+const Register = require("./routes/auth/register");
+const Login = require("./routes/auth/login");
 
+app.use("/api/auth", Register);
+app.use("/api/auth", Login);
 
 app.listen(3000, () => {
-    console.log(`Server Started at http://localhost:3000`);
-     console.log("Swagger Docs: http://localhost:3000/api-docs");
-})
-
+  console.log(`Server Started at http://localhost:3000`);
+  console.log("Swagger Docs: http://localhost:3000/api-docs");
+});
--- routes/auth/login.js
+++ routes/auth/login.js
@@ -5,7 +5,10 @@ const Role = require("../../models/Role");
 const Token = require("../../models/Token");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const { generateAccessToken,  generateRefreshToken } = require("../../jwt/generateToken");
+const {
+    generateAccessToken,
+    generateRefreshToken,
+} = require("../../jwt/generateToken");
 require("dotenv").config();
 
 /**
@@ -35,98 +38,95 @@ require("dotenv").config();
  *         description: Invalid credentials
  */
 
-
-
 router.get("/login", (req, res) => {
-  res.send("login page");
+    res.send("login page");
 });
 
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+    const {email, password} = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email and password required" });
-  }
-
-  try {
-    const user = await User.findOne({ email }).populate({
-      path: "roleId",
-      select: "name description -_id",
-    });
-
-    console.log("user info:", user);
-
-    if (!user) {
-      return res.status(401).json({ error: "Invalid credentials" });
+    if (!email || !password) {
+        return res.status(400).json({message: "Email and password required"});
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: "Invalid credentials" });
+    try {
+        const user = await User.findOne({email}).populate({
+            path: "roleId",
+            select: "name description -_id",
+        });
+
+        console.log("user info:", user);
+
+        if (!user) {
+            return res.status(401).json({error: "Invalid credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({message: "Invalid credentials"});
+        }
+
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+        console.log("this the refreshToken", refreshToken);
+        console.log("this the accessToken", accessToken);
+
+        await Token.create({userId: user.id, refreshToken: refreshToken});
+
+        res.status(200).json({
+            message: "Login successful",
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                role: user.roleId?.name || null,
+                description: user.roleId.description || null,
+            },
+        });
+    } catch (error) {
+        console.log("ERROR LOGIN USER:", error);
+        res.status(500).json({error: "Server error"});
     }
-
-    const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
-
-    
-    console.log("this the refreshToken", refreshToken)
-    console.log("this the accessToken", accessToken)
-
-    await Token.create({userId:user.id, refreshToken:refreshToken});
-    
-    res.status(200).json({
-      message: "Login successful",
-      accessToken:accessToken,
-      refreshToken:refreshToken,
-      user: {
-        id: user._id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        role: user.roleId?.name || null,
-        description:user.roleId.description || null
-      },
-    });
-  } catch (error) {
-    console.log("ERROR LOGIN USER:", error);
-    res.status(500).json({ error: "Server error" });
-  }
 });
 
 router.post("/refresh", async (req, res) => {
-    const {token} = req.body
+    const {token} = req.body;
 
-    if(!token) return res.status(401).json({ message: "Refresh token required" });
+    if (!token)
+        return res.status(401).json({message: "Refresh token required"});
 
-    try{
-      const storedToken = await Token.find({token});
+    try {
+        const storedToken = await Token.find({token});
 
-      if(!storedToken){
-        return res.status(403).json({message:"Invalid refresh token"})
-      }
+        if (!storedToken) {
+            return res.status(403).json({message: "Invalid refresh token"});
+        }
 
-         const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
-    const user = await User.findById(decoded.email);
-    if (!user) return res.status(403).json({ message: "User not found" });
+        const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+        const user = await User.findById(decoded.userId);
+        if (!user) return res.status(403).json({message: "User not found"});
 
-    const newAccessToken = generateAccessToken(user);
-    res.json({ accessToken: newAccessToken });
-    
-    }catch(err){
-       console.log(err);
-    res.status(403).json({ message: "Invalid or expired refresh token" });
+        const newAccessToken = generateAccessToken(user);
+        res.json({accessToken: newAccessToken});
+    } catch (err) {
+        console.log(err);
+        res.status(403).json({message: "Invalid or expired refresh token"});
     }
-})
+});
 
 const blacklistedTokens = new Set();
 
 router.post("/logout", (req, res) => {
-  const token = req.headers.authorization?.split(" ")[1] || req.body?.token;
+    const token = req.headers.authorization?.split(" ")[1] || req.body?.token;
 
-  if (!token) return res.status(400).json({ message: "No token provided" });
+    if (!token) return res.status(400).json({message: "No token provided"});
 
-  blacklistedTokens.add(token);
-  return res.status(200).json({ message: "Logout successful" });
+    blacklistedTokens.add(token);
+    return res.status(200).json({message: "Logout successful"});
 });
 
 module.exports = router;
